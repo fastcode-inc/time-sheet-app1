6ceaa147d2190add001f8e8225708b13b7547a59
Muhammad Usman <usman@getfastcode.com>
2021-03-26 12:35:13 +0500

    added/removed extended files -- added changeset to add versiono col

--- timesheetapp1/src/main/java/com/fastcode/timesheetapp1/application/extended/appconfiguration/IAppConfigurationAppServiceExtended.java
+++
@@ -1,8 +0,0 @@
-package com.fastcode.timesheetapp1.application.extended.appconfiguration;
-
-import com.fastcode.timesheetapp1.application.core.appconfiguration.IAppConfigurationAppService;
-
-public interface IAppConfigurationAppServiceExtended extends IAppConfigurationAppService {
-
-	//Add your custom code here
-}
